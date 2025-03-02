452bc655313a66813a6848e7c183b1546fe7bae8
Kabuddy768 <benmwangi968@gmail.com>
2025-03-02 20:04:02 +0300

    Change1

--- Typescript/1TypeAnnotations/src/1basicTypes.ts
+++ Typescript/1TypeAnnotations/src/1basicTypes.ts
@@ -43,19 +43,175 @@
 
 // console.log(validateUsername("Christian"));
 
-const appElement = document.getElementById('app')
+// const appElement = document.getElementById('app')
 
-if(!appElement){
-  throw new Error("App element not found")
+// if(!appElement){
+//   throw new Error("App element not found")
+// }
+
+
+
+function logId(id: number) {
+  console.log(`The id is ${id}`)
 }
+
+type User = {
+  id: string | number
+}
+
+const user: User = {
+  id: 123,
+}
+
+logId(123)
+
+
+
+const getAlbumYear = (year: string | number | boolean) => {
+  if (typeof year === 'string') {
+    console.log(`The album was released in ${year}.`) // `year` is string
+  } else if (typeof year === 'number') {
+    console.log(`The album was released in ${year}.`) // `year` is number | boolean
+  }
+
+  console.log(year) // `year` is string | number | boolean
+}
+
+getAlbumYear(2024)
 
 
 function sum(a: number, b: number): number {
   return a + b;
 }
 
-describe('sum function', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
+// describe('sum function', () => {
+//   test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+//   });
+// });
+
+type APIResponse =
+  | {
+      data: {
+        id: string
+      }
+    }
+  | {
+      error: string
+    }
+
+const handleResponse = (response: APIResponse) => {
+  // How do we check if 'data' is in the response?
+
+  if ('data' in response) {
+    return response.data.id
+  } else {
+    throw new Error(response.error)
+  }
+}
+
+
+
+
+
+
+// type User1 = {
+//   id: string;
+//   createdAt: Date;
+//   name: string;
+//   email: string;
+// };
+
+// interface user1 extends BaseEntity{
+//   id: string;
+//   createdAt: Date;
+//   name: string;
+//   email: string;
+// }
+
+
+// type Product = {
+//   id: string;
+//   createdAt: Date;
+//   name: string;
+//   price: number;
+// };
+
+// interface Product extends BaseEntity{
+//   id: string;
+//   createdAt: Date;
+//   name: string;
+//   price: number;
+// }
+
+// interface BaseEntity {
+//   id: string;
+//   createdAt: Date;
+// }
+// type Score={
+//   subject:string
+// }
+// const scores: {
+//   [subject:string]:number
+// }={};
+
+// scores.math = 95;
+// scores.english = 90;
+// scores.science = 85;
+
+// interface Scores {
+//   [subject:string]:number
+// }
+
+// const scores: Scores = {
+//   math: 95,
+//   english: 90,
+// };
+
+// scores.athletics = 100;
+
+// scores.french = 75;
+
+// scores.spanish = 70;
+
+// const sum1 = (...Num:number[])=>{
+//   return Num.reduce((prev,next)=>prev+next,0)
+// }
+
+// console.log(sum1(1,2,3,4,5,6,7));
+
+
+// interface StdMarks{
+//   name:string
+//   marks:number
+// }
+
+class StudentMarks{
+  name:string
+  marks:number[]
+
+  constructor(){
+    this.name="Ben Mwangi"
+    this.marks=[12,13,15]
+  }
+}
+
+const LoopMarks = new StudentMarks
+console.log(LoopMarks);
+
+interface AlbumOptions{
+  title: string;
+  artist: string;
+  releaseYear: number;
+}
+class Album {
+  title: string;
+  artist: string;
+  releaseYear: number;
+
+  constructor(opt:AlbumOptions) {
+    this.title = opt.title
+    this.artist = opt.title
+    this.releaseYear = opt.releaseYear
+  }
+}
